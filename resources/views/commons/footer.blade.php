f76af3592b63b497d700dcc731a5ab783149668c
EC2 Default User <t1dxoxo@icloud.com>
2019-12-22 09:47:08 +0000

    前回分

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,25 @@
+<footer class="page-footer font-small special-color-dark pt-4" style = "background-color: aquamarine;">
+    <div class="container">           
+        <ul class="list-unstyled list-inline text-center" style = "display: flex;">
+            <div class = twitters style="margin: 0 auto; display: flex;">
+                <div class = "p-2">
+                    <li>Nanaka's Twitter</li>
+                    <li class="list-inline-item">
+                        <a href="{{ url('https://twitter.com/nanakaglucklich') }}">
+                            <img src="https://img.icons8.com/dusk/48/000000/twitter-circled.png">
+                        </a>
+                    </li>
+                </div>
+                <div class= "p-2">
+                    <li>Riki's Twitter</li>
+                    <li class="list-inline-item">
+                        <a href = "https://twitter.com/nakamuraPHP">
+                            <img src="{{ url('https://img.icons8.com/dusk/48/000000/twitter-circled.png') }}">
+                        </a>
+                    </li>
+                </div>
+            </div>
+        </ul>
+    </div>
+    <div class="footer-copyright text-center py-3">© 2020 Nanaka&Riki All Rights Reserved.</div>
+</footer>
--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -36,9 +36,7 @@
     @yield('content')
     <footer class="page-footer font-small special-color-dark pt-4" style = "background-color: aquamarine;">
 
-            <!-- Footer Elements -->
-        <div class="container">
-            <!-- Social buttons -->
+        <div class="container">           
             <ul class="list-unstyled list-inline text-center" style = "display: flex;">
                 <div class = twitters style="margin: 0 auto; display: flex;">
                     <div class = "p-2">
@@ -58,27 +56,10 @@
                         </li>
                     </div>
                 </div>
-            <!-- <li class="list-inline-item">
-                <a class="btn-floating btn-gplus mx-1">
-                <i class="fab fa-google-plus-g"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-li mx-1">
-                <i class="fab fa-linkedin-in"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-dribbble mx-1">
-                <i class="fab fa-dribbble"> </i>
-                </a> 
-            </li>-->
             </ul>
-            <!-- Social buttons -->        
         </div>
-        <!-- Footer Elements -->
-        <!-- Copyright -->
         <div class="footer-copyright text-center py-3">© 2020 Nanaka&Riki All Rights Reserved.</div>
-    <!-- Copyright -->    
+  
+    </footer>
 </body>
 </html>
