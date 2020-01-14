ed998d036eaf92649c6a72880947033e21c2bb03
EC2 Default User <t1dxoxo@icloud.com>
2020-01-14 10:17:06 +0000

    v-calender導入

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+        if (request()->isSecure()) {
+        \URL::forceScheme('https');
+        }
     }
 
     /**
